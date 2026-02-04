01f88abfe24a3c370dc0dee9c0ab7807c5ec4d8a
kaykyjuvenal <neguito.juvenal@gmail.com>
2026-02-04 11:27:11 -0300

    feat: inital commit

---
+++ src/main/java/org/br/unipds/projetodeimplementacaojavaelite/ProjetoDeImplementacaoJavaEliteApplication.java
@@ -0,0 +1,13 @@
+package org.br.unipds.projetodeimplementacaojavaelite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeImplementacaoJavaEliteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoDeImplementacaoJavaEliteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/br/unipds/projetodeimplementacaojavaelite/ProjetoDeImplementacaoJavaEliteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.br.unipds.projetodeimplementacaojavaelite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeImplementacaoJavaEliteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
