11dbe586ea59d24e3fc2d0afd2dad24005083baa
kaykyjuvenal <neguito.juvenal@gmail.com>
2026-02-04 12:18:38 -0300

    feat: add ex1 implementation. - Simple method with my name, my age and my city,

--- src/main/java/org/br/unipds/projetodeimplementacaojavaelite/ProjetoDeImplementacaoJavaEliteApplication.java
+++ src/main/java/org/br/unipds/projetodeimplementacaojavaelite/ProjetoDeImplementacaoJavaEliteApplication.java
@@ -1,13 +1,15 @@
 package org.br.unipds.projetodeimplementacaojavaelite;
 
+import org.br.unipds.projetodeimplementacaojavaelite.aulas.Aula1ExerciciosPraticos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetoDeImplementacaoJavaEliteApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(ProjetoDeImplementacaoJavaEliteApplication.class, args);
+        Aula1ExerciciosPraticos.ex1();
     }
 
 }
--- src/main/java/org/br/unipds/projetodeimplementacaojavaelite/aulas/Aula1ExerciciosPraticos.java
+++ src/main/java/org/br/unipds/projetodeimplementacaojavaelite/aulas/Aula1ExerciciosPraticos.java
@@ -1,4 +1,9 @@
 package org.br.unipds.projetodeimplementacaojavaelite.aulas;
 
+
 public class Aula1ExerciciosPraticos {
+    public static void ex1() {
+        String texto = "Nome: Kayky Flávio Juvenal\nIdade: 22\nCidade: São Carlos\n";
+        System.out.println(texto);
+    }
 }
