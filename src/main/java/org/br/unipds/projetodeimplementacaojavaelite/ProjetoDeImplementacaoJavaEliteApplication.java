1d7f60f76fa612fb8c675eb9e36785866230a5ac
kaykyjuvenal <neguito.juvenal@gmail.com>
2026-02-04 14:18:02 -0300

    feat: add isAdult and calculadoraIMCSemParametros() funtions.

--- src/main/java/org/br/unipds/projetodeimplementacaojavaelite/ProjetoDeImplementacaoJavaEliteApplication.java
+++ src/main/java/org/br/unipds/projetodeimplementacaojavaelite/ProjetoDeImplementacaoJavaEliteApplication.java
@@ -1,6 +1,6 @@
 package org.br.unipds.projetodeimplementacaojavaelite;
 
-import org.br.unipds.projetodeimplementacaojavaelite.aulas.Aula1ExerciciosPraticos;
+import org.br.unipds.projetodeimplementacaojavaelite.aulas.Modulo0ExerciciosPraticos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +9,10 @@ public class ProjetoDeImplementacaoJavaEliteApplication {
 
     static void main(String[] args) {
         SpringApplication.run(ProjetoDeImplementacaoJavaEliteApplication.class, args);
-        Aula1ExerciciosPraticos.ex1();
-        Aula1ExerciciosPraticos.meuContato();
+        Modulo0ExerciciosPraticos.ex1();
+        Modulo0ExerciciosPraticos.meuContato();
+        System.out.println();
+        System.out.println(Modulo0ExerciciosPraticos.calculadoraIMCSemParametros());
     }
 
 }
--- src/main/java/org/br/unipds/projetodeimplementacaojavaelite/aulas/Modulo0ExerciciosPraticos.java
+++ src/main/java/org/br/unipds/projetodeimplementacaojavaelite/aulas/Modulo0ExerciciosPraticos.java
@@ -1,7 +1,7 @@
 package org.br.unipds.projetodeimplementacaojavaelite.aulas;
 
 
-public class Aula1ExerciciosPraticos {
+public class Modulo0ExerciciosPraticos {
     public static void ex1() {
         String texto = "Nome: Kayky Flávio Juvenal\nIdade: 22\nCidade: São Carlos\n";
         System.out.println(texto);
@@ -10,12 +10,23 @@ public class Aula1ExerciciosPraticos {
         String nome = "Nome: Kayky Flávio Juvenal ";
         String telefone = "| Email: neguito.juvenal@gmail.com ";
         String email = "| Telefone: 16982149333";
-        var texto = nome + telefone + email;
         System.out.println(nome);
         System.out.println(telefone);
         System.out.println(email);
         System.out.print(nome);
         System.out.print(telefone);
         System.out.print(email);
+    }
+    public static double calculadoraIMCSemParametros(){
+        double peso = 130.2;
+        double altura = 1.95;
+        return Math.round(peso / (altura * altura));
+    }
+    public String isAdult(int idade){
+        if (idade >=  18){
+            return "Acesso Permitido!";
+        }else{
+            return "Acesso Negado!";
+        }
     }
 }
